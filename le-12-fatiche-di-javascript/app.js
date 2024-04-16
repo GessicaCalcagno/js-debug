5d08b514ab054d5e1a7bdd16970e219411393339
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-04-16 20:10:55 +0200

    Fix: some arrors of the '12 labors'

--- le-12-fatiche-di-javascript/app.js
+++ le-12-fatiche-di-javascript/app.js
@@ -51,26 +51,36 @@
 // });
 
 const input = document.querySelector('input');
-const array = null;
+console.log(input);
+//Array null non ha senso al massimo può essere vuoto
+const array = [];
+console.log(array);
 
 input.addEventListener('keypress', function(event) {
 
     if (event.code != 'Enter')    return;
     if (input.value.length == '') return;
 
-    array.add(input.value);
+
+    //è push no add per aggiungere l'elemento in un array
+    array.push(input.value);
 
     const li = document.createElement('li');
     li.classList.add('list-group-item');
-    li.text = input.value;
-    document.querySelector('ul').push(li);
+    //In Js, non puoi impostare il testo di un elemento <li> utilizzando la proprietà text. Posso impostare il testo all'interno dell'elemento <li> utilizzando la proprietà textContent o innerText. 
+    li.innerText = input.value;
+    //Si pusha un elemento ma non gli elementi del DOM
+    //uso appendChild()
+    document.querySelector('ul').appendChild(li);
 
     let counter = '';
+    //Gli elementi di un array non si prendono così ??
     let item    = array[0];
     const max   = 1;
     const elems = [];
 
-    for (let i = 0; i < array; i++) {
+    //Devo indicare la lunghezza dell'array -> array.length
+    for (let i = 0; i < array.length; i++) {
         let val = array[i];
 
         if (!elems[val]) {
@@ -79,6 +89,7 @@ input.addEventListener('keypress', function(event) {
             elems[val]++;
         }
 
+        //j = i???
         for (let j = i; j < array.length; i++) {
             if (array[i] == array[j]) {
                 counter++;
@@ -91,13 +102,19 @@ input.addEventListener('keypress', function(event) {
         counter = 0;
     }
 
-    const alert = document.getElementsByClassName('alert');
+    //utilizzare querySelector() anziché getElementsByClassName() perché restituisce una NodeList e non è possibile accedere agli elementi come con un singolo elemento.
+    const alert = document.querySelector('alert');
 
+    //Dov'è Alert???
     alert.classList.remove('d-none');
     alert.classList.add('d-flex');
 
     alert.querySelector('span:first-child').innerText = item;
     alert.querySelector('span:last-child').innerText = max;
 
-    console.log('${item} trovato ${max} volte');
+    //è sbagliato prendere la variabile cosi non è tra apici ma devo usare il template literals
+    console.log(`${item} trovato ${max} volte`);
 });
+
+//LO SO CHE AVETE DETTO CHE CI AVETE PASSATO IL CODICE CORRETTO (per sbagli) è COMMENTATO SOPRA! MA COME POTETE NOTARE SONO DA UN ORA QUII A CERCARE DI CAPIRE T-T 
+//Se notate la faccina che piange mandatemi una emoji divertente su slack!
