473336a00b55f76b541c8f5db551ecb4ae40ba11
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-04-17 17:38:54 +0200

    Fix: le 12 fatiche di js

--- le-12-fatiche-di-javascript/app.js
+++ le-12-fatiche-di-javascript/app.js
@@ -50,71 +50,84 @@
 //     console.log(`${item} trovato ${max} volte`);
 // });
 
-const input = document.querySelector('input');
+const input = document.querySelector("input");
 console.log(input);
 //Array null non ha senso al massimo può essere vuoto
 const array = [];
-console.log(array);
+//console.log(array);
 
-input.addEventListener('keypress', function(event) {
+//Alt J
+input.addEventListener("keypress", function (event) {
+  //La funzione prosegue se viene premuto
+  if (event.code != "Enter") return;
+  //La funzione prosegue se il valore dell'input è diverso da ""
+  // input è un number e qui non va scritto .length
+  if (input.value == "") return;
 
-    if (event.code != 'Enter')    return;
-    if (input.value.length == '') return;
+  //è push no add per aggiungere l'elemento in un array
+  array.push(input.value);
 
+  const li = document.createElement("li");
+  li.classList.add("list-group-item");
+  //In Js, non puoi impostare il testo di un elemento <li> utilizzando la proprietà text. Posso impostare il testo all'interno dell'elemento <li> utilizzando la proprietà textContent o innerText.
+  li.innerText = input.value;
+  //Si pusha un elemento ma non gli elementi del DOM
+  //push solo per gli array
+  //solo append al posto di push
+  document.querySelector("ul").append(li);
 
-    //è push no add per aggiungere l'elemento in un array
-    array.push(input.value);
+  //* ripulisco l'input
+  input.value = "";
 
-    const li = document.createElement('li');
-    li.classList.add('list-group-item');
-    //In Js, non puoi impostare il testo di un elemento <li> utilizzando la proprietà text. Posso impostare il testo all'interno dell'elemento <li> utilizzando la proprietà textContent o innerText. 
-    li.innerText = input.value;
-    //Si pusha un elemento ma non gli elementi del DOM
-    //uso appendChild()
-    document.querySelector('ul').appendChild(li);
+  //Contatore? allora è un numero non una stringa vuota
+  let counter = 0;
+  let item = array[0];
+  /* valore massimo raggiunto da un elemento
+    const max - nella condizione coambia il proprio valore*/
+  let max = 1;
+  //Let non costante
+  let elems = [];
 
-    let counter = '';
-    //Gli elementi di un array non si prendono così ??
-    let item    = array[0];
-    const max   = 1;
-    const elems = [];
+  //Devo indicare la lunghezza dell'array -> array.length
+  for (let i = 0; i < array.length; i++) {
+    let val = array[i];
 
-    //Devo indicare la lunghezza dell'array -> array.length
-    for (let i = 0; i < array.length; i++) {
-        let val = array[i];
+    //Se val non è presente in elems = 1 altrimenti incrementa
+    if (!elems[val]) {
+      elems[val] = 1;
+    } else {
+      elems[val]++;
+    }
+    // console.log(elems);
 
-        if (!elems[val]) {
-            elems[val] = 1;
-        } else {
-            elems[val]++;
+    // ciclo per controllare se un elemento è presente altre volte
+    // j++ non i++!!
+    for (let j = i; j < array.length; j++) {
+      // per ogni elemento nell'array uguale a quello preso in considerazione da i il counter aumenta
+      if (array[i] == array[j]) {
+        counter++;
+        if (max < counter) {
+          max = counter;
+          item = array[i];
         }
-
-        //j = i???
-        for (let j = i; j < array.length; i++) {
-            if (array[i] == array[j]) {
-                counter++;
-                if (max < counter)
-                max  = counter;
-                item = array[i];
-            }
-        }
-
-        counter = 0;
+      }
     }
 
-    //utilizzare querySelector() anziché getElementsByClassName() perché restituisce una NodeList e non è possibile accedere agli elementi come con un singolo elemento.
-    const alert = document.querySelector('alert');
+    counter = 0;
+  }
 
-    //Dov'è Alert???
-    alert.classList.remove('d-none');
-    alert.classList.add('d-flex');
+//   const alert = document.getElementsByClassName("alert")[0];
+  //Selezioare l'elemento con il numero con il counter più alto
+  //.getElementsByClassName andava bene pure
+  const alert = document.querySelector('.alert');
 
-    alert.querySelector('span:first-child').innerText = item;
-    alert.querySelector('span:last-child').innerText = max;
+  alert.classList.remove("d-none");
+  alert.classList.add("d-flex");
 
-    //è sbagliato prendere la variabile cosi non è tra apici ma devo usare il template literals
-    console.log(`${item} trovato ${max} volte`);
+  alert.querySelector("span:first-child").innerText = item;
+  alert.querySelector("span:last-child").innerText = max;
+
+  //è sbagliato prendere la variabile cosi
+  console.log(`${item} trovato ${max} volte`);
 });
 
-//LO SO CHE AVETE DETTO CHE CI AVETE PASSATO IL CODICE CORRETTO (per sbagli) è COMMENTATO SOPRA! MA COME POTETE NOTARE SONO DA UN ORA QUII A CERCARE DI CAPIRE T-T 
-//Se notate la faccina che piange mandatemi una emoji divertente su slack!
